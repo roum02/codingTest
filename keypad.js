aa5615e9dea21effbc90333e7f98f69b5ddae7a5
roum02 <roum02@ewhain.net>
2021-09-05 23:37:30 +0900

    keypad(2)

--- keypad.js
+++ keypad.js
@@ -1,5 +1,22 @@
 function solution(numbers, hand) {
-  var answer = String(numbers).replace(/1|4|7/g, "L").replace(/3|6|9/g, "R");
+  const keypad = {
+    1: [0, 3],
+    2: [1, 3],
+    3: [2, 3],
+    4: [0, 2],
+    5: [1, 2],
+    6: [2, 2],
+    7: [0, 1],
+    8: [1, 1],
+    9: [2, 1],
+    "*": [0, 0],
+    0: [1, 0],
+    "#": [2, 0],
+  };
+
+  //거리 계산
+  function getDistance() {}
+  // var answer = String(numbers).replace(/1|4|7/g, "L").replace(/3|6|9/g, "R");
   return answer;
 }
 
