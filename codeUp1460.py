8094e02be9114537269bf9d4e2869d39485ed36a
roum02 <roum02@ewhain.net>
2021-09-08 23:53:39 +0900

    codeUp1460

---
+++ codeUp1460.py
@@ -0,0 +1,13 @@
+def solution(n):
+  inner = []
+  outer = []
+
+  for i in range(1,n+1):
+    inner.append(i)
+    for i in range(1, n+1):
+      outer.append(inner)
+    
+
+  return outer
+
+print(solution(3))
--- plus_minus.js
+++ plus_minus.js
@@ -1,8 +1,15 @@
+// function solution(absolutes, signs) {
+//   var answer = absolutes.map((x, index) => {
+//     return signs[index] ? x : -x;
+//   });
+//   return answer.reduce((a, b) => a + b);
+// }
+
 function solution(absolutes, signs) {
-  var answer = absolutes.map((x, index) => {
-    return signs[index] ? x : -x;
+  var result = absolutes.reduce((acc, val, i) => {
+    console.log(acc, val, i);
+    return acc + val * (signs[i] ? 1 : -1), 0;
   });
-  return answer.reduce((a, b) => a + b);
 }
 
 console.log(solution([4, 7, 12], [true, false, true]));
