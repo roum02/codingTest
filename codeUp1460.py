a13b416fe14831ae07899e8b912d89f68f77d365
roum02 <roum02@ewhain.net>
2021-09-10 23:39:37 +0900

    0910

--- codeUp1460.py
+++ codeUp1460.py
@@ -1,13 +1,11 @@
 def solution(n):
   inner = []
   outer = []
-  c = 0
 
   for i in range(1,n+1):
-    inner.append(i+c)
+    inner.append(i)
     for i in range(1, n+1):
       outer.append(inner)
-    c += 3
   return outer
 
 print(solution(3))
---
+++ training.js
@@ -0,0 +1,6 @@
+function solution(n, lost, reserve) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(5, [2, 4], [1, 3, 5]);
