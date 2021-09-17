0614d9a20eab1ba2b9645cebd9f2fa198b68deee
roum02 <roum02@ewhain.net>
2021-09-17 23:56:09 +0900

    2588

---
+++ 2588.js
@@ -0,0 +1,10 @@
+// const fs = require("fs");
+// var input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+// var a = parseInt(input[0]);
+// var b = parseInt(input[1]);
+
+var a = 472;
+var b = 385;
+
+console.log(Number(b.toString().substr(-1, 1)) * a);
