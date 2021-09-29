ead6b2a021c7df75762d97d56f01a4908a62d689
roum02 <roum02@ewhain.net>
2021-09-29 23:19:00 +0900

    14681

--- 14681.js
+++ 14681.js
@@ -1,14 +1,28 @@
-const fs = require("fs");
-var input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-var a = Number(input[0]);
-var b = Number(input[1]);
-
-// var a = 9;
-// var b = -13;
+let input = [];
 
 function Quadrant(a, b) {
   return a > 0 && b > 0 ? 1 : a < 0 && b > 0 ? 2 : a < 0 && b < 0 ? 3 : 4;
 }
+
+rl.on("line", function (line) {
+  input.push(parseInt(line));
+}).on("close", function () {
+  ///////////////////////////////
+  const x = input[0];
+  const y = input[1];
+
+  console.log(Quadrant(x, y));
+  /////////////////////////////
+  process.exit();
+});
+
+// var a = 9;
+// var b = -13;
 
 console.log(Quadrant(a, b));
