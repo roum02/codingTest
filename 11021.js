80fee2281131608eab62ad6493a7c9d02de9808d
roum02 <roum02@ewhain.net>
2021-10-09 11:24:07 +0900

    11021

---
+++ 11021.js
@@ -0,0 +1,9 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let answer = "";
+
+for (let i = 1; i <= input[0]; i++) {
+  let numbers = input[i].split(" ");
+  answer += `Case #${i}: ${Number(numbers[0]) + Number(numbers[1]) + "\n"}`;
+}
+
+console.log(answer);
