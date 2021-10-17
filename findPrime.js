90a852499ead0d90c49c91725cceef38019653bc
roum02 <roum02@ewhain.net>
2021-10-17 23:47:06 +0900

    findPrime(3)

--- findPrime.js
+++ findPrime.js
@@ -1,4 +1,4 @@
-// 완전탐색 알고리즘
+//완전탐색 알고리즘
 
 function solution(numbers) {
   let answer = [];
@@ -32,3 +32,32 @@ function solution(numbers) {
 }
 
 console.log(solution("011"));
+
+function getNumbeOfCases(numbers) {
+  // 중복을 막기 위해서
+  const result = new Set();
+
+  // 재귀 함수를 통해 만든다.
+  const temp = (currFix, eachArr) => {
+    for (let i = 0; i < eachArr.length; i++) {
+      const tempEachArr = [...eachArr];
+      const tempCurrFixVal = tempEachArr.splice(i, 1)[0];
+      const tempCurrFix = currFix + tempCurrFixVal;
+      result.add(Number(tempCurrFix));
+      if (tempEachArr.length > 0) temp(tempCurrFix, tempEachArr);
+    }
+  };
+
+  // 시작
+  for (let i = 0; i < numbers.length; i++) {
+    let target = numbers[i];
+    result.add(Number(target));
+
+    const eachArr = [...numbers];
+    eachArr.splice(i, 1);
+
+    temp(target, eachArr);
+  }
+
+  return new Array(...result);
+}
