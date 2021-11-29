eb7d9ecac0f95c7a8c07417f6b2a0fc1dec9617d
roum02 <roum02@ewhain.net>
2021-11-29 23:01:22 +0900

    1129

--- greedy/4.py
+++ greedy/4.py
@@ -7,11 +7,36 @@ arr = [2,3,1,2,2]
 count = 0
 
 arr.sort()
-new_arr = arr
+print(arr) #[1, 2, 2, 2, 3]
+#new_arr = arr
 
-i = 0
-while i<len(new_arr):
-  new_arr = [x for x in arr if x not in arr[:new_arr[i]]]
-  print(new_arr)
-  print(new_arr[:new_arr[i]])
-  i += 1
+for i in range(len(arr)):
+  #new_arr = [x for x in arr if x not in arr[:new_arr[i]]]
+  for j in range(i+1, arr[i]):
+    if arr[i] < arr[j]:
+      break
+    else:
+      count += 1
+      i += arr[i]
+    print(i)
+
+print(count)
+
+## 풀이
+## 한 줄에 공백을 기준으로 입력
+n = int(input())
+data = list(map(int, input.split()))
+data.sort()
+
+result = 0 #총 그룹 수
+count = 0 #현재 그룹에 포함된 모험가의 수
+
+for i in data:
+  count += 1
+  if count >= i:
+    result += 1
+    count = 0
+
+print(result)
+
+  
