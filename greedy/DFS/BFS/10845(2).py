185b17d3ee2a904e5c811d5fc92d09ef53e2c1b1
roum02 <roum02@ewhain.net>
2022-01-22 00:47:18 +0900

    백준 queue

---
+++ greedy/DFS/BFS/10845(2).py
@@ -0,0 +1,57 @@
+import sys 
+from collections import deque 
+
+n = 15
+data = [
+  'push 1',
+'push 2',
+'front',
+'back',
+'size',
+'empty',
+'pop',
+'pop',
+'pop',
+'size',
+'empty',
+'pop',
+'push 3',
+'empty',
+'front'
+]
+
+queue = deque([]) 
+
+#for _ in range(int(sys.stdin.readline().strip())):
+for command in data:
+ #command = sys.stdin.readline().strip()
+ if command[0]=='s': # size 
+   print(len(queue)) 
+   print('size:', queue)
+ elif command[0]=='e': # empty 
+  if queue: 
+    print(0)
+    print('empty', queue)
+    continue 
+  print(1) 
+ elif command[0]=='f': # front 
+   if queue: 
+     print(queue[0]) 
+     print('front:', queue)
+     continue
+   print(-1) 
+ elif command[0]=='b': # back 
+   if queue: 
+     print(queue[-1]) 
+     print('back:', queue)
+     continue
+   print(-1) 
+ elif command[1]=='o': # pop 
+   if queue: 
+     print(queue.popleft())
+     print('pop:', queue)
+     continue 
+   print(-1) 
+ else: 
+   queue.append(command[5:])
+   print('push',queue)
---
+++ greedy/DFS/BFS/10845.py
@@ -0,0 +1,68 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+data = [sys.stdin.readline().strip() for i in range(n)]
+
+# n = 15
+# data = [
+#   'push 1',
+# 'push 2',
+# 'front',
+# 'back',
+# 'size',
+# 'empty',
+# 'pop',
+# 'pop',
+# 'pop',
+# 'size',
+# 'empty',
+# 'pop',
+# 'push 3',
+# 'empty',
+# 'front'
+# ]
+
+
+queue = deque([])
+
+for i in data:
+  if 'push' in i:
+    push_num = int(i[-1])
+    queue.append(push_num)
+    #print('push',queue)
+  elif 'pop' in i:
+    if queue:
+      pop_num = queue.popleft()
+      print(pop_num)
+      #print('pop:', queue)
+    else:
+      print(-1)
+      #print('pop:', queue)
+  elif 'size' in i:
+    print(len(queue))
+    #print('size:', queue)
+  elif 'empty' in i:
+    if queue:
+      print(0)
+      #print('empty', queue)
+    else:
+      print(1)
+      #print('empty', queue)
+  elif 'front' in i:
+    if not queue:
+      print(-1)
+    else:
+      print(queue[0])
+      #print('front', queue)
+  elif 'back' in i:
+    if not queue:
+      print(-1)
+    else:
+      print(queue[-1])
+      #print('back', queue)
+
+
+
+
+
